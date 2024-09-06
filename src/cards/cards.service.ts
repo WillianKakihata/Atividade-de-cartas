e018d52b055fc4c5061bad726d8334277e17e620
WillianKakihata <161545901+WillianKakihata@users.noreply.github.com>
2024-09-05 21:22:18 -0300

    fix. randomizar as cartas sem repeticao

--- src/cards/cards.service.ts
+++ src/cards/cards.service.ts
@@ -7,42 +7,44 @@ export class CardsService {
 
     async generate(): Promise<CreateCardsDto> {
         const cardCommander = await this.apigetCommander();
-        const commanderName = cardCommander.name;
-        const cards = await this.get99NonLegendaryCards(cardCommander.colors || []);
-        const cardNames = cards.map(card => card.name);
+        const commanderName = this.getCardName(cardCommander);
+        const nonLegendaryCards = await this.get99NonLegendaryCards(cardCommander.colors || []);
+        const cardNames = nonLegendaryCards.map(this.getCardName);
     
-        const randomCards = {
+        return {
           cardCommander: commanderName,
           cards: cardNames,
         };
-    
-        return randomCards;
       }
-
-    async apigetCommander(): Promise<any> {
+    
+      private async apigetCommander(): Promise<any> {
         const response: AxiosResponse = await axios.get(
           'https://api.magicthegathering.io/v1/cards?supertypes=legendary',
         );
         const commanderCards = response.data.cards;
-        const randomCommander =
-          commanderCards[Math.floor(Math.random() * commanderCards.length)];
-        return randomCommander;
+        return commanderCards[Math.floor(Math.random() * commanderCards.length)];
       }
-
-    async get99NonLegendaryCards(colors: string[]): Promise<any[]> {
+    
+      private async get99NonLegendaryCards(colors: string[]): Promise<any[]> {
         const colorQuery = colors.join(',');
         const response: AxiosResponse = await axios.get(
           `https://api.magicthegathering.io/v1/cards?colors=${colorQuery}&supertypes!=legendary`,
         );
         const nonLegendaryCards = response.data.cards;
+        
+        return this.getRandomCards(nonLegendaryCards, 99);
+      }
     
-        const randomNonLegendaryCards = [];
-        for (let i = 0; i < 99; i++) {
-          const randomCard =
-            nonLegendaryCards[Math.floor(Math.random() * nonLegendaryCards.length)];
-          randomNonLegendaryCards.push(randomCard);
+      private getCardName(card: any): string {
+        return card.name;
+      }
+    
+      private getRandomCards(cards: any[], count: number): any[] {
+        const randomCards = [];
+        for (let i = 0; i < count; i++) {
+          const randomCard = cards[Math.floor(Math.random() * cards.length)];
+          randomCards.push(randomCard);
         }
-    
-        return randomNonLegendaryCards;
+        return randomCards;
       }
 }
